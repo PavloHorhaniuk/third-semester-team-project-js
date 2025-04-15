4c064000cafd43d74da5c4a1b14eaefcdb7d289f
David201027 <156106393+David201027@users.noreply.github.com>
2025-04-15 12:53:09 +0300

    leap-year scss

--- src/js/leapYear.js
+++ src/js/leapYear.js
@@ -1,23 +1,23 @@
 function checkLeapYear() {
-    const year = parseInt(document.getElementById("yearInput").value);
+    const year = parseInt(document.getElementById("year").value);
     let result = "";
 
     if (isNaN(year)) {
       result = "Введіть коректний рік";
     } else if (year % 4 !== 0) {
-      result = year + " — не високосний рік";
+      result = "Ви народилися не у високосний рік!";
     } else if (year % 100 !== 0) {
-      result = year + " — високосний рік";
+      result = "Ви народилися у високосний рік!";
     } else if (year % 400 === 0) {
-      result = year + " — високосний рік";
+      result = "Ви народилися у високосний рік!";
     } else {
-      result = year + " — не високосний рік";
+      result = "Ви народилися не у високосний рік!";
     }
 
-    document.getElementById("result").textContent = result;
+    document.getElementById("leap-year__result").textContent = result;
 }
 
-const buttonCheck = document.getElementById('checkYear');
+const buttonCheck = document.getElementById('leap-year__btn');
 
 buttonCheck.addEventListener("click", function() {
     checkLeapYear();
