1cad87b60a674d89bca4299c68d8de4d18338b4a
lovin4ik <122011576+lovin4ik@users.noreply.github.com>
2025-04-28 20:09:12 +0300

    add filterHeader

---
+++ src/js/headerSorting.js
@@ -0,0 +1,29 @@
+const filterBtns = document.querySelectorAll('#btnFilter')
+const sections = document.querySelectorAll('section')
+
+const withoutFilter = () => {
+	sections.forEach(section => {
+		section.style.display = 'block'
+		console.log(section.style.display)
+	})
+}
+
+const filterSections = data => {
+	if (data === 'without-filter') {
+		withoutFilter()
+	} else {
+		sections.forEach(section => {
+			const dataSection = section.dataset.section
+			section.style.display = 'none'
+			if (dataSection === data) {
+				section.style.display = 'block'
+			}
+		})
+	}
+}
+
+filterBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		filterSections(btn.dataset.filter)
+	})
+})
