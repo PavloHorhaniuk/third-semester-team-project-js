a9d39dc801a7453faaabf324b4a42428b264d68c
lovin4ik <122011576+lovin4ik@users.noreply.github.com>
2025-04-30 13:26:06 +0300

    update

--- src/js/ourTeam.js
+++ src/js/ourTeam.js
@@ -1,25 +1,28 @@
-const slider = document.querySelectorAll('.our-team__list li');
-const sliderList = document.querySelector(".our-team__list");
-let currentPosition = 0;
-const arrowLeft = document.querySelector('.our-team__arrleft');
-const arrowRight = document.querySelector('.our-team__arrright');
+const slider = document.querySelectorAll('[data-slider-item]')
+const sliderList = document.querySelector('[data-slider]')
+let currentPosition = 0
+const arrowLeft = document.querySelector('.our-team__arrleft')
+const arrowRight = document.querySelector('.our-team__arrright')
 
-const slide = 150;
+const slideWidth = 180
 
-arrowRight.addEventListener( "click",()=>{
-  currentPosition ++;
-  if (currentPosition >= slider.length){
-    currentPosition = 0;
-  }  
-  moveSlider();
-});
-arrowLeft.addEventListener( "click",()=>{
-  currentPosition --;
-  if (currentPosition < 0){
-    currentPosition = slider.length -1;
-  }
-  moveSlider();
-});
-function moveSlider(){
-  sliderList.style.left = `-${currentPosition * slide}px`;
+arrowRight.addEventListener('click', () => {
+	currentPosition++
+	if (currentPosition >= slider.length) {
+		currentPosition = 0
+	}
+	moveSlider()
+})
+arrowLeft.addEventListener('click', () => {
+	currentPosition--
+	if (currentPosition < 0) {
+		currentPosition = slider.length - 1
+	}
+	moveSlider()
+})
+
+function moveSlider() {
+	slider.forEach(slide => {
+		slide.style.transform = `translateX(-${currentPosition * slideWidth}px)`
+	})
 }
