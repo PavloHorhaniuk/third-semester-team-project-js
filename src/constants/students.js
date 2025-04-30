f077ac2924382ebb4bc4b27619934e1b0976bd36
lovin4ik <122011576+lovin4ik@users.noreply.github.com>
2025-04-30 14:19:48 +0300

    update

---
+++ src/constants/students.js
@@ -0,0 +1,8 @@
+const students = [
+	{
+		id: 1,
+		name: 'Валера',
+		info: 'Team lead, Зробив: Хедер, Гугл диназаврика, і поміг з нашою командою',
+		avatar: 'placeholder'
+	}
+]
--- src/js/ourTeam.js
+++ src/js/ourTeam.js
@@ -1,10 +1,41 @@
-const slider = document.querySelectorAll('[data-slider-item]')
-const sliderList = document.querySelector('[data-slider]')
-let currentPosition = 0
+const sliderDataName = 'data-slider'
+const slider = document.querySelectorAll(`[${sliderDataName}-item]`)
+const sliderList = document.querySelector(`[${sliderDataName}]`)
+
+const pagination = document.querySelector(`[${sliderDataName}-pagination]`)
+
 const arrowLeft = document.querySelector('.our-team__arrleft')
 const arrowRight = document.querySelector('.our-team__arrright')
 
 const slideWidth = 180
+const paginationBtnElement = `<li class="our-team__pagination-item"><button data-slider-pagination-btn class=""></button></li>`
+
+let currentPosition = 0
+
+for (let i = 0; i < slider.length; i++) {
+	pagination.insertAdjacentHTML('beforeend', paginationBtnElement)
+}
+
+const paginationBtns = document.querySelectorAll(
+	`[${sliderDataName}-pagination-btn]`
+)
+
+paginationBtns.forEach((btn, index) => {
+	btn.addEventListener('click', () => {
+		currentPosition = index
+		moveSlider()
+		updatePagination()
+	})
+})
+
+setInterval(() => {
+	currentPosition++
+	if (currentPosition >= slider.length) {
+		currentPosition = 0
+	}
+	moveSlider()
+	updatePagination()
+}, 10000)
 
 arrowRight.addEventListener('click', () => {
 	currentPosition++
@@ -12,13 +43,16 @@ arrowRight.addEventListener('click', () => {
 		currentPosition = 0
 	}
 	moveSlider()
+	updatePagination()
 })
+
 arrowLeft.addEventListener('click', () => {
 	currentPosition--
 	if (currentPosition < 0) {
 		currentPosition = slider.length - 1
 	}
 	moveSlider()
+	updatePagination()
 })
 
 function moveSlider() {
@@ -26,3 +60,16 @@ function moveSlider() {
 		slide.style.transform = `translateX(-${currentPosition * slideWidth}px)`
 	})
 }
+
+function updatePagination() {
+	paginationBtns.forEach((paginationBtn, i) => {
+		if (i === currentPosition) {
+			paginationBtn.classList.add('active')
+		} else {
+			paginationBtn.classList.remove('active')
+		}
+	})
+}
+
+updatePagination()
+moveSlider()
