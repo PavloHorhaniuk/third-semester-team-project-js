7e5fe9eb89d445dd743facb3a4dead59b39aeae2
Michael Shpeh <mspeg236@gmail.com>
2025-05-03 14:06:12 +0300

    js for guess-number

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,27 +1,27 @@
-const openModalButton = document.querySelector("[data-modal-open]");
-const closeModalButton = document.querySelector("[data-modal-close]");
-const modalBackdrop = document.querySelector("[data-modal]");
+const openModalButton = document.querySelector('[data-modal-open]')
+const closeModalButton = document.querySelector('[data-modal-close]')
+const modalBackdrop = document.querySelector('[data-modal]')
 
 openModalButton.addEventListener('click', () => {
-  modalBackdrop.classList.remove('is-hidden');
-  document.body.classList.add('no-scroll');
-});
+	modalBackdrop.classList.remove('is-hidden')
+	document.body.classList.add('no-scroll')
+})
 
-closeModalButton.addEventListener('click', closeModal);
+closeModalButton.addEventListener('click', closeModal)
 
-modalBackdrop.addEventListener('click', (e) => {
-  if (e.target === modalBackdrop) {
-    closeModal();
-  }
-});
+modalBackdrop.addEventListener('click', e => {
+	if (e.target === modalBackdrop) {
+		closeModal()
+	}
+})
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    closeModal();
-  }
-});
+document.addEventListener('keydown', e => {
+	if (e.key === 'Escape') {
+		closeModal()
+	}
+})
 
 function closeModal() {
-  modalBackdrop.classList.add('is-hidden');
-  document.body.classList.remove('no-scroll');
+	modalBackdrop.classList.add('is-hidden')
+	document.body.classList.remove('no-scroll')
 }
